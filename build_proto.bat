6967c9bf0b9423c2bfe0767620a04794ded4a534
EC-K <elxone.19jt@gmail.com>
2025-08-02 11:10:07 +0900

    chore: update build_proto.bat

--- build_proto.bat
+++ build_proto.bat
@@ -9,6 +9,11 @@ protoc -I=%PROTO_PATH% --python_out=./mediapipe_inferencer/src/proto_generated %
 type nul > .\\mediapipe_inferencer\\src\\proto_generated\\__init__.py
 
 echo Generating C# code...
-protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/PoseInference %PROTO_PATH%/holistic_landmarks.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/landmark.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/kinect_pose_landmarks.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/pose_landmarks.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/hand_landmarks.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/face_results.proto
+protoc -I=%PROTO_PATH% --csharp_out=./c#_runtime/MpAndKinectPoseSender/Proto %PROTO_PATH%/holistic_landmarks.proto
 
 echo Done.
