189610615d98a46f42324a2aab2bb0302bca35ac
EC-K <elxone.19jt@gmail.com>
2025-08-07 17:13:16 +0900

    refactor: divide processes in main as methods

--- c#_runtime/KinectPoseInferencer/AppManager.cs
+++ c#_runtime/KinectPoseInferencer/AppManager.cs
@@ -2,9 +2,10 @@
 // Released under the MIT license
 // https://github.com/microsoft/Azure-Kinect-Samples/blob/master/LICENSE
 
+using K4AdotNet.BodyTracking;
 using K4AdotNet.Record;
 using K4AdotNet.Sensor;
-using K4AdotNet.BodyTracking;
+using KinectPoseInferencer.Input;
 using KinectPoseInferencer.PoseInference;
 using KinectPoseInferencer.Renderers;
 using System;
@@ -14,8 +15,11 @@ namespace KinectPoseInferencer
 {
     internal class AppManager
     {
+        LandmarkHandler _landmarkHandler;
+
         public void RunOfflineProcess(string filePath)
         {
+            // Read the file
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($"Error: {filePath} is not found.");
@@ -23,24 +27,24 @@ namespace KinectPoseInferencer
             }
             Console.WriteLine($"Reading a .mkv file '{filePath}' ...");
 
+            // Initialize playeback and tracker
             var playback = new Playback(filePath);
+            RecordConfiguration recordConfig;
+            Calibration calibration;
+            playback.GetRecordConfiguration(out recordConfig);
+            playback.GetCalibration(out calibration);
+            var tracker = new Tracker(
+                calibration
+                , new TrackerConfiguration
+                {
+                    SensorOrientation = SensorOrientation.Default,
+                    ProcessingMode = TrackerProcessingMode.Gpu,
+                    GpuDeviceId = 0,
+                    ModelPath = null
+                });
+
             try
             {
-                RecordConfiguration recordConfig;
-                Calibration calibration;
-                playback.GetRecordConfiguration(out recordConfig);
-                playback.GetCalibration(out calibration);
-                var tracker = new Tracker(
-                    calibration
-                    , new TrackerConfiguration
-                    {
-                        SensorOrientation = SensorOrientation.Default,
-                        ProcessingMode = TrackerProcessingMode.Gpu,
-                        GpuDeviceId = 0,
-                        ModelPath = null
-                    });
-
-                int frameCount = 0;
                 while (true)
                 {
                     Capture capture;
@@ -51,21 +55,14 @@ namespace KinectPoseInferencer
                         break;
                     }
 
-                    frameCount++;
-                    Console.WriteLine($"フレーム {frameCount} を処理中...");
-
                     tracker.EnqueueCapture(capture);
                     capture.Dispose();
 
-                    var frame = tracker.PopResult();
-
-
-                    if (frame.BodyCount > 0)
+                    using var frame = tracker.PopResult();
+                    if (frame is not null)
                     {
-                        Skeleton skeleton;
-                        frame.GetBodySkeleton(0, out skeleton);
-                        //landmarkHandler.Update(skeleton);
-                        //landmarkHandler.SendResults();
+                        Inference(frame, new Action<Skeleton>[] { });
+
                     }
                     frame.Dispose();
                 }
@@ -117,8 +114,19 @@ namespace KinectPoseInferencer
             PointCloud.ComputePointCloudCache(deviceCalibration);
             var tiltCorrector = new TiltCorrector(imuSample, deviceCalibration);
             using var landmarkHandler = new LandmarkHandler(tiltCorrector);
+            _landmarkHandler = landmarkHandler;
 
-            var userInputChar = "-";
+
+            var actionMap = new ActionMap();
+            var keyInputProvider = new KeyInputProvider();
+            var userActionService = new UserActionService(
+                actionMap,
+                keyInputProvider,
+                new UserAction(landmarkHandler));
+            var landmarkHandlingActions = new Action<Skeleton>[]
+            {
+                SendLandmarks
+            };
             while (renderer.IsActive)
             {
                 using (Capture sensorCapture = device.GetCapture())
@@ -136,45 +144,51 @@ namespace KinectPoseInferencer
 
                 if (Console.KeyAvailable)
                 {
-                    userInputChar = Console.ReadKey().Key.ToString();
-                    if (userInputChar == "C")
-                        landmarkHandler.UpdateTiltRotation();
-                    if (userInputChar == "R")
-                        landmarkHandler.ResetTiltRotation();
+                    keyInputProvider.ReadInputAndNotify();
                 }
 
                 // Try getting latest tracker frame.
                 using var frame = tracker.PopResult();
-                if (frame != null)
+                if (frame is not null)
                 {
                     visualizerData.Frame = frame.DuplicateReference();
-
-
-                    // Write color image to thw Memory Mapped File
-                    try
-                    {
-                        var colorImg = frame.Capture.ColorImage;
-                        if (colorImg != null)
-                        {
-                            var bgraArr = colorImg.GetSpan<byte>().ToArray();
-                            imgWriter.Write(bgraArr);
-                        }
-                    }
-                    catch
-                    {
-                        continue;
-                    }
-
-                    // Send Landmarks to a pose solver app
-                    if (frame.BodyCount > 0)
-                    {
-                        Skeleton skeleton;
-                        frame.GetBodySkeleton(0, out skeleton);
-                        landmarkHandler.Update(skeleton);
-                        landmarkHandler.SendResults();
-                    }
+                    WriteColorImageToSharedMemory(frame, imgWriter);
+                    Inference(frame, landmarkHandlingActions);
                 }
             }
+        }
+
+
+
+        void WriteColorImageToSharedMemory(BodyFrame frame, ImageWriter imgWriter)
+        {
+            try
+            {
+                var colorImg = frame.Capture.ColorImage;
+                if (colorImg is not null)
+                {
+                    var bgraArr = colorImg.GetSpan<byte>().ToArray();
+                    imgWriter.Write(bgraArr);
+                }
+            }
+            catch { }
+        }
+
+        void Inference(BodyFrame frame, Action<Skeleton>[] actions)
+        {
+            if (frame.BodyCount > 0)
+            {
+                Skeleton skeleton;
+                frame.GetBodySkeleton(0, out skeleton);
+                foreach (var action in actions)
+                    action?.Invoke(skeleton);
+            }
+        }
+
+        void SendLandmarks(Skeleton skeleton)
+        {
+            _landmarkHandler.Update(skeleton);
+            _landmarkHandler.SendResults();
         }
     }
 }
