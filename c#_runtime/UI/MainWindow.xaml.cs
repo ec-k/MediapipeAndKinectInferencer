7cb7a9184ed18319e73ae34d0f11d1bffed7eb94
EC-K <elxone.19jt@gmail.com>
2025-11-24 17:03:08 +0900

    art: revise MainWindow.xaml to improve visibility

--- c#_runtime/UI/MainWindow.xaml.cs
+++ c#_runtime/UI/MainWindow.xaml.cs
@@ -1,10 +1,37 @@
-﻿namespace KinectPoseInferencer.UI;
+﻿using System.Windows;
+
+namespace KinectPoseInferencer.UI;
 
 public partial class MainWindow
 {
+    const double VideoWindowIdealAspectRatio = 16.0 / 9.0;
+
     public MainWindow(MainWindowViewModel viewModel)
     {
         InitializeComponent();
         this.DataContext = viewModel;
+
+        VideoWindowBorder.SizeChanged += OnVideoWindowSizeChanged;
+    }
+
+    void OnVideoWindowSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        var containerWidth = e.NewSize.Width;
+        var containerHeight = e.NewSize.Height;
+        var containerAspectRatio = containerWidth / containerHeight;
+
+        if (containerAspectRatio > VideoWindowIdealAspectRatio)
+        {
+            var idealWidth = containerHeight * VideoWindowIdealAspectRatio;
+            VideoWindowViewBox.MaxHeight = double.PositiveInfinity;
+            VideoWindowViewBox.MaxWidth = idealWidth;
+            
+        }
+        else
+        {
+            var idealHeight = containerWidth / VideoWindowIdealAspectRatio;
+            VideoWindowViewBox.MaxHeight = idealHeight;
+            VideoWindowViewBox.MaxWidth = double.PositiveInfinity;
+        }
     }
 }
