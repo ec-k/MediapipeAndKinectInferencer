e40ce8a66d47f6a9d5dffe0aabce37602e0565e8
EC-K <elxone.19jt@gmail.com>
2025-12-06 19:03:06 +0900

    refactor: change field type to ReactiveProperty

--- c#_runtime/KinectPoseInferencer/FrameCaptureBroker.cs
+++ c#_runtime/KinectPoseInferencer/FrameCaptureBroker.cs
@@ -2,61 +2,52 @@ using K4AdotNet.BodyTracking;
 using K4AdotNet.Sensor;
 using System;
 using KinectPoseInferencer.Playback;
+using R3;
 
 namespace KinectPoseInferencer;
 
 public class FrameCaptureBroker : IDisposable
 {
-    public event Action<Capture, BodyFrame> OnNewFrameReady;
     public event Action<IInputLogEvent>? OnNewInputLogEvent;
 
-    Capture _currentCapture;
-    BodyFrame _currentBodyFrame;
+    public ReadOnlyReactiveProperty<Capture> Capture => _capture;
+    public ReadOnlyReactiveProperty<BodyFrame> Frame => _frame;
 
-    public void ProcessNewFrame(Capture capture, BodyFrame bodyFrame)
+    ReactiveProperty<Capture> _capture = new();
+    ReactiveProperty<BodyFrame> _frame = new();
+
+    /// <summary>
+    /// Assign duplicated reference of the given capture to the broker.
+    /// </summary>
+    /// <param name="capture"></param>
+    public void UpdateCapture(Capture capture)
     {
-        // Dispose existing frames if any
-        _currentCapture?.Dispose();
-        _currentBodyFrame?.Dispose();
-
-        // Hold onto the new frames
-        // It's the responsibility of the caller (PlaybackReader) to ensure these are DuplicateReference() if needed
-        // for internal processing after passing them to the broker.
-        _currentCapture = capture;
-        _currentBodyFrame = bodyFrame;
-
-        // Notify subscribers that a new frame pair is ready
-        OnNewFrameReady?.Invoke(_currentCapture, _currentBodyFrame);
+        // Dispose existing capture if any
+        _capture?.CurrentValue?.Dispose();
+        _capture.Value = capture.DuplicateReference();
     }
 
-    public void ProcessNewInputLogEvent(IInputLogEvent inputLogEvent) // Add InputLogEvent processing
+    /// <summary>
+    /// Assign duplicated reference of the given body frame to the broker.
+    /// </summary>
+    /// <param name="bodyFrame"></param>
+    public void UpdateBodyFrame(BodyFrame bodyFrame)
+    {
+        // Dispose existing body frame if any
+        _frame?.CurrentValue?.Dispose();
+        _frame.Value = bodyFrame.DuplicateReference();
+    }
+
+    public void ProcessNewInputLogEvent(IInputLogEvent inputLogEvent)
     {
         OnNewInputLogEvent?.Invoke(inputLogEvent);
     }
 
-    /// <summary>
-    /// Provides a DuplicateReference of the current Capture to the consumer.
-    /// The consumer is responsible for disposing the returned Capture.
-    /// </summary>
-    public Capture GetCurrentCaptureDuplicate()
-    {
-        return _currentCapture?.DuplicateReference();
-    }
-
-    /// <summary>
-    /// Provides a DuplicateReference of the current BodyFrame to the consumer.
-    /// The consumer is responsible for disposing the returned BodyFrame.
-    /// </summary>
-    public BodyFrame GetCurrentBodyFrameDuplicate()
-    {
-        return _currentBodyFrame?.DuplicateReference();
-    }
-
     public void Dispose()
     {
-        _currentCapture?.Dispose();
-        _currentBodyFrame?.Dispose();
-        _currentCapture = null;
-        _currentBodyFrame = null;
+        _capture?.Dispose();
+        _frame?.Dispose();
+        _capture = null;
+        _frame = null;
     }
 }
