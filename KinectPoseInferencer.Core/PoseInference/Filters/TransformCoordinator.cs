c25e5da8f41e28612aa5e83c08cae3947abc7e90
EC-K <elxone.19jt@gmail.com>
2026-01-28 12:22:06 +0900

    feat(filter): add converting joint rotation in TransformCoordinator

--- KinectPoseInferencer.Core/PoseInference/Filters/TransformCoordinator.cs
+++ KinectPoseInferencer.Core/PoseInference/Filters/TransformCoordinator.cs
@@ -1,19 +1,35 @@
 ﻿using HumanLandmarks;
+using System.Numerics;
 
 namespace KinectPoseInferencer.Core.PoseInference.Filters;
 
 public class TransformCoordinator: ILandmarkFilter
 {
+    private static readonly Quaternion _upsideDownCorrection
+        = Quaternion.CreateFromAxisAngle(Vector3.UnitZ, (float)Math.PI);
+
     public Landmark Apply(in Landmark landmark, float timestamp)
-        => new Landmark
+    {
+        var rotationConverted = new Rotation
         {
-            Position = new Position
-            {
-                X = -landmark.Position.X,
-                Y = -landmark.Position.Y,
-                Z = -landmark.Position.Z
-            },
-            Rotation = landmark.Rotation,
-            Confidence = landmark.Confidence,
-        };
+            X = landmark.Rotation.X,
+            Y = -landmark.Rotation.Y,
+            Z = -landmark.Rotation.Z,
+            W = landmark.Rotation.W
+        }.ToQuaternion();
+        rotationConverted = _upsideDownCorrection * rotationConverted;
+
+        return new Landmark
+           {
+               Position = new Position
+               {
+                   X = -landmark.Position.X,
+                   Y = -landmark.Position.Y,
+                   Z = -landmark.Position.Z
+               },
+               // Negate the imaginary part of the quaternion to reflect the axis inversion
+               Rotation = rotationConverted.ToRotation(),
+               Confidence = landmark.Confidence,
+           };
+    }
 }
